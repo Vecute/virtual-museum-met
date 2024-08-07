13a2004a7e66d9204b93c8b2f9d9fbfaea02c76e
Gleb Khramtsov <vecutemoffy@gmail.com>
2024-08-07 15:02:04 +0300

    fix

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import { Link, useSearchParams, useNavigate } from "react-router-dom";
 import SearchPagination from "../utilities/SearchPagination";
 import TemplatePage from "./TemplatePage";
+import { fetchDepartments } from "../thunk/fetchDepartments";
+import { RootState, useAppDispatch } from "../redux/store";
+import { useSelector } from "react-redux";
 
 const API_BASE_URL = "https://api.artic.edu/api/v1/artworks/search";
 
@@ -10,34 +13,98 @@ interface Artwork {
   title: string;
   image_id: string;
   department_title: string;
+  place_of_origin: string;
+  date_start: number;
+  date_end: number;
+}
+
+interface DepartmentOption {
+  id: string;
+  title: string;
 }
 
 const SearchPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const navigate = useNavigate();
-  const [searchTerm, setSearchTerm] = useState(
-    searchParams.get("query[term][department_id]") || ""
-  );
+  const [searchTerm, setSearchTerm] = useState(searchParams.get("q") || "");
+  const [placeOfOriginFilter, setPlaceOfOriginFilter] = useState("");
+  const [dateStartFilter, setDateStartFilter] = useState("");
+  const [dateEndFilter, setDateEndFilter] = useState("");
   const [results, setResults] = useState<Artwork[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(
     parseInt(searchParams.get("page") || "1", 10) || 1
   );
   const [totalPages, setTotalPages] = useState(1);
+  const [departmentOptions, setDepartmentOptions] = useState<DepartmentOption[]>(
+    []
+  );
+  const dispatch = useAppDispatch();
+  const departments = useSelector(
+    (state: RootState) => state.departmentsReducer.departments
+  );
 
-  // Функция для обновления параметров поиска
+    // Устанавливаем initialDepartmentId из URL
+    const initialDepartmentId = searchParams.get("query[term][department_id]");
+
+    // Инициализируем departmentFilter с initialDepartmentId
+    const [departmentFilter, setDepartmentFilter] = useState(initialDepartmentId || "");
+
   const updateSearchParams = (newParams: { [key: string]: string }) => {
-    setSearchParams((prevParams) => { // Используем setSearchParams с колбэком
-      navigate(`?${new URLSearchParams({...prevParams, ...newParams }).toString()}`);
-      return { ...prevParams, ...newParams }; 
+    setSearchParams((prevParams) => {
+      navigate(
+        `?${new URLSearchParams({ ...prevParams, ...newParams }).toString()}`
+      );
+      return { ...prevParams, ...newParams };
     });
   };
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
     setCurrentPage(1);
+    updateSearchParams({ q: event.target.value, page: "1" });
+  };
+
+  const handleDepartmentFilterChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setDepartmentFilter(event.target.value);
+    setCurrentPage(1);
     updateSearchParams({
       "query[term][department_id]": event.target.value,
+      page: "1",
+    });
+  };
+
+  const handlePlaceOfOriginFilterChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setPlaceOfOriginFilter(event.target.value);
+    setCurrentPage(1);
+    updateSearchParams({
+      "query[term][place_of_origin]": event.target.value,
+      page: "1",
+    });
+  };
+
+  const handleDateStartFilterChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setDateStartFilter(event.target.value);
+    setCurrentPage(1);
+    updateSearchParams({
+      "query[range][date_start]": `{ "gte": ${event.target.value} }`,
+      page: "1",
+    });
+  };
+
+  const handleDateEndFilterChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setDateEndFilter(event.target.value);
+    setCurrentPage(1);
+    updateSearchParams({
+      "query[range][date_end]": `{ "lte": ${event.target.value} }`,
       page: "1",
     });
   };
@@ -48,39 +115,140 @@ const SearchPage = () => {
   };
 
   useEffect(() => {
+    // Создаем промис, который будет разрешен после установки departmentFilter
+    const departmentFilterPromise = new Promise<void>((resolve) => {
+      const initialDepartmentId = searchParams.get("query[term][department_id]");
+      if (initialDepartmentId) {
+        setDepartmentFilter(initialDepartmentId);
+      }
+      resolve(); // Разрешаем промис после установки departmentFilter
+    });
+
+    // useEffect для загрузки данных
     const fetchData = async () => {
+      // Ждем разрешения промиса, прежде чем продолжить
+      await departmentFilterPromise;
+
       setIsLoading(true);
       const url = new URL(API_BASE_URL);
       url.searchParams.set("limit", "20");
       url.searchParams.set("page", currentPage.toString());
-      url.searchParams.set("fields", "id,title,image_id,department_title");
+      url.searchParams.set(
+        "fields",
+        "id,title,image_id,department_title,place_of_origin,date_start,date_end"
+      );
+
+      // Construct filter parameters for Elasticsearch query
+      const filter = [];
       if (searchTerm) {
-        url.searchParams.set("query[term][department_id]", searchTerm);
+        filter.push({ match: { title: searchTerm } });
+      }
+      if (departmentFilter) {
+        filter.push({ term: { department_id: departmentFilter } });
+      }
+      if (placeOfOriginFilter) {
+        filter.push({ term: { place_of_origin: placeOfOriginFilter } });
+      }
+      if (dateStartFilter) {
+        filter.push({ range: { date_start: { gte: dateStartFilter } } });
+      }
+      if (dateEndFilter) {
+        filter.push({ range: { date_end: { lte: dateEndFilter } } });
       }
 
+      // Construct the request body with filter
+      const requestBody = {
+        query: { bool: { must: filter } },
+      };
+
       try {
-        const response = await fetch(url.toString());
+        const response = await fetch(url.toString(), {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(requestBody),
+        });
+
         const data = await response.json();
+
         setResults(data.data);
         setTotalPages(data.pagination.total_pages);
       } catch (error) {
-        console.error("Ошибка при получении данных:", error);
+        console.error("Error fetching data:", error);
       } finally {
         setIsLoading(false);
       }
     };
 
     fetchData();
-  }, [searchTerm, currentPage]); 
+  }, [
+    searchTerm,
+    departmentFilter,
+    placeOfOriginFilter,
+    dateStartFilter,
+    dateEndFilter,
+    currentPage,
+    searchParams,
+  ]);
+
+  useEffect(() => {
+    dispatch(fetchDepartments());
+    setDepartmentOptions(departments);
+  }, [dispatch]);
+
+  const uniquePlacesOfOrigin = [
+    ...new Set(results.map((result) => result.place_of_origin)),
+  ];
 
   return (
     <TemplatePage title="Search through the collection">
       <div>
+        {/* Search Input */}
         <input
           type="text"
           value={searchTerm}
           onChange={handleSearchChange}
-          placeholder="Search by department..."
+          placeholder="Search..."
+        />
+
+        {/* Department Filter */}
+        <select value={departmentFilter} onChange={handleDepartmentFilterChange}>
+          <option value="">All Departments</option>
+          {departmentOptions.map((department) => (
+            <option key={department.id} value={department.id}>
+              {department.title}
+            </option>
+          ))}
+        </select>
+
+        {/* Place of Origin Filter */}
+        <select
+          value={placeOfOriginFilter}
+          onChange={handlePlaceOfOriginFilterChange}
+        >
+          <option value="">All Places of Origin</option>
+          {uniquePlacesOfOrigin.map((place) => (
+            <option key={place} value={place}>
+              {place}
+            </option>
+          ))}
+        </select>
+
+        {/* Start Date Filter */}
+        <input
+          type="number"
+          value={dateStartFilter}
+          onChange={handleDateStartFilterChange}
+          placeholder="Start Date"
+        />
+
+        {/* End Date Filter */}
+        <input
+          type="number"
+          value={dateEndFilter}
+          onChange={handleDateEndFilterChange}
+          placeholder="End Date"
         />
 
         {isLoading && (
@@ -107,6 +275,7 @@ const SearchPage = () => {
             </Link>
           ))}
         </div>
+
         {!isLoading && (
           <SearchPagination
             totalPages={totalPages}
--- src/thunk/fetchDepartments.ts
+++ src/thunk/fetchDepartments.ts
@@ -1,79 +1,105 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
+// Определение интерфейса для объекта Department
 export interface Department {
   id: string;
   title: string;
 }
 
+// Ключ для хранения данных в localStorage
 const departmentsCacheKey = "departmentsCache";
 
+// Создание асинхронного thunk с помощью createAsyncThunk
 export const fetchDepartments = createAsyncThunk(
+  // Тип действия для thunk
   "departments/fetchDepartments",
+  // Асинхронная функция, которая будет вызываться
   async () => {
+    let cachedData;
     try {
-      const cachedData = await localStorage.getItem(departmentsCacheKey);
-      if (cachedData) {
-        const parsedData: Department[] = JSON.parse(cachedData);
-        return parsedData;
-      }
+      // Попытка получить данные из localStorage
+      cachedData = localStorage.getItem(departmentsCacheKey);
     } catch (error) {
-      console.error("Ошибка парсинга данных из localStorage:", error);
+      // Логирование ошибки чтения из localStorage
+      console.error("Ошибка чтения из localStorage:", error);
     }
 
-    try {
-      const response = await fetch(
-        "https://api.artic.edu/api/v1/artworks/search",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            "aggs": {
-              "departments": {
-                "terms": {
-                  "field": "department_id",
-                  "size": 10000,
-                },
-                "aggs": { // Добавляем вложенный агрегатор top_hits
-                  "top_hit": {
-                    "top_hits": {
-                      "size": 1,
-                      "_source": ["department_title"],
-                    },
+    // Если данные есть в localStorage
+    if (cachedData) {
+      try {
+        // Попытка распарсить данные из JSON
+        return JSON.parse(cachedData);
+      } catch (error) {
+        // Логирование ошибки парсинга данных из localStorage
+        console.error("Ошибка парсинга данных из localStorage:", error);
+      }
+    }
+
+    // Отправка POST запроса к API Art Institute of Chicago
+    const response = await fetch(
+      "https://api.artic.edu/api/v1/artworks/search",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // Тело запроса с агрегацией для получения departments
+        body: JSON.stringify({
+          aggs: {
+            departments: {
+              // Агрегация по полю department_id
+              terms: {
+                field: "department_id",
+                // Получаем все department_id (до 10000)
+                size: 10000,
+              },
+              // Вложенный агрегатор top_hits для получения department_title
+              aggs: {
+                top_hit: {
+                  top_hits: {
+                    // Получаем только один результат
+                    size: 1,
+                    // Запрашиваем только поле department_title
+                    _source: ["department_title"],
                   },
                 },
               },
             },
-            "_source": [], // Не нужно запрашивать _source для всего документа
-          }),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Ошибка загрузки данных");
+          },
+          // Не нужно запрашивать _source для всего документа
+          _source: [],
+        }),
       }
+    );
 
-      const data = await response.json();
-
-      // Извлекаем данные о departments, используя top_hits для получения department_title
-      const departments: Department[] = data.aggregations.departments.buckets.map(
-        (bucket: any) => ({
-          id: bucket.key,
-          title: bucket.top_hit.hits.hits[0]._source.department_title,
-        })
-      );
-
-      try {
-        localStorage.setItem(departmentsCacheKey, JSON.stringify(departments));
-      } catch (error) {
-        console.error("Ошибка сохранения данных в localStorage:", error);
-      }
-
-      return departments;
-    } catch (error) {
-      console.error("Ошибка при получении данных:", error);
-      throw error;
+    // Проверка, был ли ответ успешным
+    if (!response.ok) {
+      // Если ответ не успешный, выбрасываем ошибку
+      throw new Error("Ошибка загрузки данных");
     }
+
+    // Парсинг JSON ответа
+    const data = await response.json();
+
+    // Извлечение данных о departments, используя top_hits для получения department_title
+    const departments = data.aggregations.departments.buckets.map(
+      (bucket: any) => ({
+        // id department
+        id: bucket.key,
+        // title department, с использованием опциональной цепочки для безопасного доступа
+        title: bucket.top_hit.hits.hits[0]?._source?.department_title,
+      })
+    );
+
+    try {
+      // Попытка сохранить данные в localStorage
+      localStorage.setItem(departmentsCacheKey, JSON.stringify(departments));
+    } catch (error) {
+      // Логирование ошибки записи в localStorage
+      console.error("Ошибка записи в localStorage:", error);
+    }
+
+    // Возвращение массива departments
+    return departments;
   }
 );
